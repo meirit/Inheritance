0ac02abf0df33ec5e42927941309fcce86758a52
meirit <meiritaserdikauskaite@gmail.com>
2023-10-24 19:49:25 +0300

    Add method doSomethingA in mainGitDemo

--- src/main/java/lt/meirita/git_demo/MainGitDemo.java
+++ src/main/java/lt/meirita/git_demo/MainGitDemo.java
@@ -3,6 +3,11 @@ package lt.meirita.git_demo;
 public class MainGitDemo {
     public static void main(String[] args) {
 
+        doSomethingA();
+
+    }
+
+    private static void doSomethingA() {
     }
 
 }
